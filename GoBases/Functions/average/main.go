3bb5398d2744d8ad319c903813fc9b887c0e1869
lgaione <livia.gaione@mercadolivre.com>
2025-05-29 16:28:53 -0300

    :sparkles: feat: exercice functions - calculate grade average

---
+++ GoBases/Functions/average/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var grade int
+
+	var sum int
+
+	fmt.Printf("Insira a quantidade de notas: ")
+
+	fmt.Scanf("%d", &grade)
+
+	for i := range grade {
+
+		var value int
+
+		fmt.Print("Insira a nota do aluno: ", i+1)
+
+		fmt.Scan(&value)
+
+		sum += value
+	}
+
+	media := average(grade, sum)
+	fmt.Printf("A média das notas é: %d\n", media)
+}
+
+func average(grade int, value int) int {
+
+	sum := value / grade
+
+	return sum
+
+}
